31a10d22724f2a847fd5ac360692b0d89ee47774
Jarad Gray <jarad.h.gray@gmail.com>
2021-04-08 15:49:06 -0500

    implement ViewModelBase class

---
+++ GestureDrawingTimer/viewmodels/ViewModelBase.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace GestureDrawingTimer.viewmodels
+{
+    class ViewModelBase : INotifyPropertyChanged
+    {
+        // Implement notifying PropertyChanged functionality
+        public event PropertyChangedEventHandler PropertyChanged; // required by INotifyPropertyChanged interface
+        // The method we'll call when we want to raise the PropertyChanged event
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
