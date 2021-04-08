867eccda72b1512af53b15f4c0055566dee12274
Jarad Gray <jarad.h.gray@gmail.com>
2021-04-08 17:22:01 -0500

    implement selecting a folder and syncing with view

--- GestureDrawingTimer/MainWindow.xaml.cs
+++ GestureDrawingTimer/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using GestureDrawingTimer.viewmodels;
 
 namespace GestureDrawingTimer
 {
@@ -20,9 +21,39 @@ namespace GestureDrawingTimer
     /// </summary>
     public partial class MainWindow : Window
     {
+        // Instance variables
+        private MainWindowViewModel mViewModel;
+
+
+        // Constructor
         public MainWindow()
         {
             InitializeComponent();
+
+            // Get a ViewModel
+            mViewModel = new MainWindowViewModel();
+
+            // Handle changes to ViewModel's state
+            mViewModel.PropertyChanged += (sender, args) =>
+            {
+                switch (args.PropertyName)
+                {
+                    case "SelectedFolderPath":
+                        folderTextBlock.Text = $"Folder: {mViewModel.SelectedFolderPath}";
+                        break;
+                }
+            };
+
+            // Initialize views to ViewModel's state
+            folderTextBlock.Text = $"Folder: {mViewModel.SelectedFolderPath}";
+        }
+
+
+        // Private methods
+
+        private void SelectFolderButton_Click(object sender, RoutedEventArgs e)
+        {
+            mViewModel.SelectFolderButton_Click();
         }
     }
 }
--- GestureDrawingTimer/viewmodels/MainWindowViewModel.cs
+++ GestureDrawingTimer/viewmodels/MainWindowViewModel.cs
@@ -6,7 +6,51 @@ using System.Threading.Tasks;
 
 namespace GestureDrawingTimer.viewmodels
 {
-    class MainWindowViewModel
+    class MainWindowViewModel : ViewModelBase
     {
+        // Properties and their backing fields
+
+        private string _selectedFolderPath;
+        public string SelectedFolderPath
+        {
+            get { return _selectedFolderPath; }
+            private set
+            {
+                if (value != null && !value.Equals(_selectedFolderPath))
+                {
+                    _selectedFolderPath = value;
+                    OnPropertyChanged();
+                    // TODO get images in selected folder and subfolders
+                }
+            }
+        }
+
+
+        // Constructor
+        public MainWindowViewModel()
+        {
+            // Initialize DirPath from persisted data
+            SelectedFolderPath = Properties.Settings.Default.SelectedFolderPath;
+        }
+
+
+        // Public methods
+
+        public void SelectFolderButton_Click()
+        {
+            // Show an OpenFileDialog (because .NET doesn't have a select folder dialog)
+            //  and get the selected file's directory
+            var ofd = new Microsoft.Win32.OpenFileDialog();
+            ofd.InitialDirectory = SelectedFolderPath; // set ofd's initial directory to persisted SelectedFolderPath
+            if (ofd.ShowDialog() == true)
+            {
+                string path = System.IO.Path.GetDirectoryName(ofd.FileName);
+                // persist selected folder's path
+                Properties.Settings.Default.SelectedFolderPath = path;
+                Properties.Settings.Default.Save(); // persist settings across application settings
+                // update member
+                SelectedFolderPath = path;
+            }
+        }
     }
 }
