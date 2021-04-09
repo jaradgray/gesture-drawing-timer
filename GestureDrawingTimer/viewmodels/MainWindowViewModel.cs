0c79192fe89d4380f13ed4f29f54483ef0f73488
Jarad Gray <jarad.h.gray@gmail.com>
2021-04-08 20:40:53 -0500

    get image paths in selected folder and its subdirectories

--- GestureDrawingTimer/viewmodels/MainWindowViewModel.cs
+++ GestureDrawingTimer/viewmodels/MainWindowViewModel.cs
@@ -20,10 +20,19 @@ namespace GestureDrawingTimer.viewmodels
                 {
                     _selectedFolderPath = value;
                     OnPropertyChanged();
-                    // TODO get images in selected folder and subfolders
+
+                    // TODO update image-related members
+                    //  - list of image paths
+                    //  - number of images (elements in image paths list)
+                    //  - number of subfolders
+                    UpdateImagePaths();
                 }
             }
         }
+
+
+        // Private variables
+        private List<string> mImagePaths = new List<string>();
 
 
         // Constructor
@@ -51,6 +60,54 @@ namespace GestureDrawingTimer.viewmodels
                 // update member
                 SelectedFolderPath = path;
             }
+        }
+
+
+        // Private methods
+
+        private void UpdateImagePaths()
+        {
+            // Get paths of all supported image files in the currently selected folder and all of its subfolders
+            string[] validExtensions = { ".jpg", ".jpeg", ".png", ".bmp" };
+            mImagePaths = GetFilePathsInDirectory(SelectedFolderPath, true, validExtensions);
+
+            // TODO update members
+            string msg = "Images found:\n";
+            foreach (string str in mImagePaths)
+            {
+                msg += "\n" + str;
+            }
+            System.Windows.MessageBox.Show(msg);
+        }
+
+        /// <summary>
+        /// Returns a List of all files in the directory at the given path whose extension matches one of those
+        /// in @validExtensions, or all files if @validExtensions is null. Includes files from all subfolders
+        /// if @doRecursive is true
+        /// </summary>
+        /// <param name="dirPath"></param>
+        /// <param name="doRecursive"></param>
+        /// <param name="validExtensions"></param>
+        /// <returns></returns>
+        private List<string> GetFilePathsInDirectory(string dirPath, bool doRecursive, string[] validExtensions = null)
+        {
+            List<string> result = new List<string>();
+
+            string[] allFiles = System.IO.Directory.GetFiles(dirPath, "*", doRecursive ? System.IO.SearchOption.AllDirectories : System.IO.SearchOption.TopDirectoryOnly);
+            // add to result files whose extension is in the validExtensions array
+            foreach (string file in allFiles)
+            {
+                foreach (string ext in validExtensions)
+                {
+                    if (ext.ToLowerInvariant().Equals(System.IO.Path.GetExtension(file).ToLowerInvariant()))
+                    {
+                        result.Add(file);
+                        break;
+                    }
+                }
+            }
+
+            return result;
         }
     }
 }
