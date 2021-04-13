90d450ad369f338a1726804999e4423092b59ac5
Jarad Gray <jarad.h.gray@gmail.com>
2021-04-13 18:41:45 -0500

    create Session class

---
+++ GestureDrawingTimer/models/Session.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestureDrawingTimer.models
+{
+    // Represents a drawing session, during which an images are displayed for a length of time
+    class Session
+    {
+
+    }
+}
