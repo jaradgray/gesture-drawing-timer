b181c7c954b6bf4d2d88e12c2f091e812db38b04
Jarad Gray <jarad.h.gray@gmail.com>
2021-04-14 15:44:43 -0500

    move GestureDrawingTimer/viewmodels/BaseINPC.cs -> GestureDrawingTimer/BaseINPC.cs

--- GestureDrawingTimer/BaseINPC.cs
+++ GestureDrawingTimer/BaseINPC.cs
@@ -1,7 +1,7 @@
 ﻿using System.ComponentModel;
 using System.Runtime.CompilerServices;
 
-namespace GestureDrawingTimer.viewmodels
+namespace GestureDrawingTimer
 {
     class BaseINPC : INotifyPropertyChanged
     {
