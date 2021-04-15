b3dc983b7708cba80dc6fc0e047e043327c579ca
Jarad Gray <jarad.h.gray@gmail.com>
2021-04-15 14:59:04 -0500

    implement Session class

--- GestureDrawingTimer/models/Session.cs
+++ GestureDrawingTimer/models/Session.cs
@@ -6,9 +6,68 @@ using System.Threading.Tasks;
 
 namespace GestureDrawingTimer.models
 {
-    // Represents a drawing session, during which an images are displayed for a length of time
-    class Session
+    // Represents a drawing session, during which images are displayed for a length of time
+    public class Session : BaseINPC
     {
+        public enum SessionState
+        {
+            Init,
+            Started,
+            Finished
+        }
 
+
+        // Properties and their backing fields
+        private SessionState _state;
+        public SessionState State
+        {
+            get { return _state; }
+            private set
+            {
+                if (value == _state) return;
+                _state = value;
+                OnPropertyChanged();
+            }
+        }
+        private int _interval;
+        public int Interval
+        {
+            get { return _interval; }
+            set
+            {
+                if (value == _interval) return;
+                _interval = value;
+                OnPropertyChanged();
+            }
+        }
+        private List<string> _imagePaths;
+        public List<string> ImagePaths
+        {
+            get { return _imagePaths; }
+            set
+            {
+                if (value == _imagePaths) return;
+                _imagePaths = value;
+                OnPropertyChanged();
+            }
+        }
+        private int _currentImageIndex;
+        public int CurrentImageIndex
+        {
+            get { return _currentImageIndex; }
+            private set
+            {
+                if (value == _currentImageIndex) return;
+                _currentImageIndex = value;
+                OnPropertyChanged();
+            }
+        }
+
+        // Constructor
+        public Session()
+        {
+            State = SessionState.Init;
+            CurrentImageIndex = 0;
+        }
     }
 }
